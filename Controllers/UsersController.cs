46d6cc336e2e0dbb0e9ca89722238fdae7d37489
Ole DUFOUR-EXT <ole.dufour-ext@fnacdarty.com>
2018-10-09 14:07:39 +0200

    changes

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,143 +1,143 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using WebApi.Services;
-using WebApi.Dtos;
-using AutoMapper;
-using System.IdentityModel.Tokens.Jwt;
-using WebApi.Helpers;
-using Microsoft.Extensions.Options;
-using System.Text;
-using Microsoft.IdentityModel.Tokens;
-using System.Security.Claims;
-using WebApi.Entities;
-using Microsoft.AspNetCore.Authorization;
+﻿//using System;
+//using System.Collections.Generic;
+//using Microsoft.AspNetCore.Mvc;
+//using WebApi.Services;
+//using WebApi.Dtos;
+//using AutoMapper;
+//using System.IdentityModel.Tokens.Jwt;
+//using WebApi.Helpers;
+//using Microsoft.Extensions.Options;
+//using System.Text;
+//using Microsoft.IdentityModel.Tokens;
+//using System.Security.Claims;
+//using WebApi.Entities;
+//using Microsoft.AspNetCore.Authorization;
 
-namespace WebApi.Controllers
-{
-    [Authorize]
-    [Route("[controller]")]
-    public class UsersController : Controller
-    {
-        private IUserService _userService;
-        private IMapper _mapper;
-        private readonly AppSettings _appSettings;
+//namespace WebApi.Controllers
+//{
+//    [Authorize]
+//    [Route("[controller]")]
+//    public class UsersController : Controller
+//    {
+//        private IUserService _userService;
+//        private IMapper _mapper;
+//        private readonly AppSettings _appSettings;
 
-        public UsersController(
-            IUserService userService,
-            IMapper mapper,
-            IOptions<AppSettings> appSettings)
-        {
-            _userService = userService;
-            _mapper = mapper;
-            _appSettings = appSettings.Value;
-        }
+//        public UsersController(
+//            IUserService userService,
+//            IMapper mapper,
+//            IOptions<AppSettings> appSettings)
+//        {
+//            _userService = userService;
+//            _mapper = mapper;
+//            _appSettings = appSettings.Value;
+//        }
 
-        [AllowAnonymous]
-        [HttpPost("authenticate")]
-        public IActionResult Authenticate([FromBody]UserDto userDto)
-        {
-           var user = _userService.Authenticate(userDto.Username, userDto.Password);
-            user.FirstName = "a";
-            user.LastName= "a";
+//        [AllowAnonymous]
+//        [HttpPost("authenticate")]
+//        public IActionResult Authenticate([FromBody]UserDto userDto)
+//        {
+//           var user = _userService.Authenticate(userDto.Username, userDto.Password);
+//            user.FirstName = "a";
+//            user.LastName= "a";
             
 
-            if (userDto.Username !="a" || userDto.Password != "a" )
-                return BadRequest("Username or password is incorrect");
+//            if (userDto.Username !="a" || userDto.Password != "a" )
+//                return BadRequest("Username or password is incorrect");
 
-            var tokenHandler = new JwtSecurityTokenHandler();
+//            var tokenHandler = new JwtSecurityTokenHandler();
           
-            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[] 
-                {
-                    new Claim(ClaimTypes.Name, user.Id.ToString())
+//            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+//            var tokenDescriptor = new SecurityTokenDescriptor
+//            {
+//                Subject = new ClaimsIdentity(new Claim[] 
+//                {
+//                    new Claim(ClaimTypes.Name, user.Id.ToString())
                    
-                }),
-                Expires = DateTime.UtcNow.AddDays(7),
+//                }),
+//                Expires = DateTime.UtcNow.AddDays(7),
                
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
+//                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+//            };
+//            var token = tokenHandler.CreateToken(tokenDescriptor);
             
-            var tokenString = tokenHandler.WriteToken(token);
+//            var tokenString = tokenHandler.WriteToken(token);
             
-            // return basic user info (without password) and token to store client side
-            return Ok(new {
-                Id = user.Id,
-                Username = user.Username,
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                Token = tokenString
-            });
-        }
+//            // return basic user info (without password) and token to store client side
+//            return Ok(new {
+//                Id = user.Id,
+//                Username = user.Username,
+//                FirstName = user.FirstName,
+//                LastName = user.LastName,
+//                Token = tokenString
+//            });
+//        }
 
-        [AllowAnonymous]
-        [HttpPost("register")]
-        public IActionResult Register([FromBody]UserDto userDto)
-        {
-            // map dto to entity
-            var user = _mapper.Map<User>(userDto);
+//        [AllowAnonymous]
+//        [HttpPost("register")]
+//        public IActionResult Register([FromBody]UserDto userDto)
+//        {
+//            // map dto to entity
+//            var user = _mapper.Map<User>(userDto);
 
-            try 
-            {
-                // save 
-                _userService.Create(user, userDto.Password);
-                return Ok();
-            } 
-            catch(AppException ex)
-            {
-                // return error message if there was an exception
-                return BadRequest(ex.Message);
-            }
-        } 
-
-
-        #region Miscellaneous
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var users = _userService.GetAll();
-            var userDtos = _mapper.Map<IList<UserDto>>(users);
-            return Ok(userDtos);
-        }
+//            try 
+//            {
+//                // save 
+//                _userService.Create(user, userDto.Password);
+//                return Ok();
+//            } 
+//            catch(AppException ex)
+//            {
+//                // return error message if there was an exception
+//                return BadRequest(ex.Message);
+//            }
+//        } 
 
 
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            var user = _userService.GetById(id);
-            var userDto = _mapper.Map<UserDto>(user);
-            return Ok(userDto);
-        }
+//        #region Miscellaneous
+//        [HttpGet]
+//        public IActionResult GetAll()
+//        {
+//            var users = _userService.GetAll();
+//            var userDtos = _mapper.Map<IList<UserDto>>(users);
+//            return Ok(userDtos);
+//        }
 
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, [FromBody]UserDto userDto)
-        {
-            // map dto to entity and set id
-            var user = _mapper.Map<User>(userDto);
-            user.Id = id;
 
-            try
-            {
-                // save 
-                _userService.Update(user, userDto.Password);
-                return Ok();
-            }
-            catch (AppException ex)
-            {
-                // return error message if there was an exception
-                return BadRequest(ex.Message);
-            }
-        }
+//        [HttpGet("{id}")]
+//        public IActionResult GetById(int id)
+//        {
+//            var user = _userService.GetById(id);
+//            var userDto = _mapper.Map<UserDto>(user);
+//            return Ok(userDto);
+//        }
 
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            _userService.Delete(id);
-            return Ok();
-        } 
-        #endregion
-    }
-}
+//        [HttpPut("{id}")]
+//        public IActionResult Update(int id, [FromBody]UserDto userDto)
+//        {
+//            // map dto to entity and set id
+//            var user = _mapper.Map<User>(userDto);
+//            user.Id = id;
+
+//            try
+//            {
+//                // save 
+//                _userService.Update(user, userDto.Password);
+//                return Ok();
+//            }
+//            catch (AppException ex)
+//            {
+//                // return error message if there was an exception
+//                return BadRequest(ex.Message);
+//            }
+//        }
+
+//        [HttpDelete("{id}")]
+//        public IActionResult Delete(int id)
+//        {
+//            _userService.Delete(id);
+//            return Ok();
+//        } 
+//        #endregion
+//    }
+//}
