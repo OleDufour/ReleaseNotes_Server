e63ae488f55515d2bbb786281b6d2342f72dfaa6
Ole DUFOUR-EXT <ole.dufour-ext@fnacdarty.com>
2018-10-30 11:19:42 +0100

    Beter Config

--- Controllers/ConfigController.cs
+++ Controllers/ConfigController.cs
@@ -22,21 +22,36 @@ namespace WebApi.Controllers
 
         // GET: api/Config
         [HttpGet]
-        public List<IConfig> GetAll()
+        public List<object> GetAll()
         {
             List<IConfig> l = new List<IConfig>();
 
-            var release = _context.Release  as IEnumerable<IConfig>;
+            var release = _context.Release as IEnumerable<IConfig>;
             var countryCode = _context.CountryCode as IEnumerable<IConfig>;
             var environment = _context.Environment as IEnumerable<IConfig>;
             var cleType = _context.CleType as IEnumerable<IConfig>;
-            
+
             l.AddRange(release.ToList());
             l.AddRange(countryCode.ToList());
             l.AddRange(environment.ToList());
             l.AddRange(cleType.ToList());
-            
-            return l;
+
+            var release2 = _context.Release;
+            var countryCode2 = _context.CountryCode;
+            var environment2 = _context.Environment;
+            var cleType2 = _context.CleType;
+
+            List<object> l2 = release2.Cast<object>()
+               .Concat(countryCode2)
+               .Concat(environment2)
+               .Concat(cleType2)
+               .ToList();
+          
+
+
+
+
+            return l2;
         }
 
     }
