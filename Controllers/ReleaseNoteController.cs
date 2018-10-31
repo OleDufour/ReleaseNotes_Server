e0b43a27691debaa9319b7e11583ddb027e252af
Ole DUFOUR-EXT <ole.dufour-ext@fnacdarty.com>
2018-10-31 16:15:11 +0100

    PostReleaseNote

--- Controllers/ReleaseNoteController.cs
+++ Controllers/ReleaseNoteController.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using WebApi.Models;
+
 
 namespace WebApi.Controllers
 {
@@ -47,71 +50,72 @@ namespace WebApi.Controllers
         }
 
         // PUT: api/ReleaseNote/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutReleaseNote([FromRoute] int id, [FromBody] ReleaseNote ReleaseNote)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutReleaseNote([FromRoute] int id, [FromBody] ReleaseNote2 ReleaseNote)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            if (id != ReleaseNote.Id)
-            {
-                return BadRequest();
-            }
+        //    if (id != ReleaseNote.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(ReleaseNote).State = EntityState.Modified;
+        //    _context.Entry(ReleaseNote).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ReleaseNoteExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!ReleaseNoteExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
-
-        // POST: api/ReleaseNote
-        [HttpPost]
-        public async Task<IActionResult> PostReleaseNote([FromBody] ReleaseNote ReleaseNote)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _context.ReleaseNote.Add(ReleaseNote);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetReleaseNote", new { id = ReleaseNote.Id }, ReleaseNote);
-        }
+        //    return NoContent();
+        //}
 
         // POST: api/ReleaseNote
         [HttpPost]
-        public async Task<IActionResult> PostReleaseNotes([FromBody] List<ReleaseNote> ReleaseNotes)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            foreach (ReleaseNote rn in ReleaseNotes)
-            {
-                _context.ReleaseNote.Add(rn);
-                await _context.SaveChangesAsync();
-            }
+        public async Task<IActionResult> PostReleaseNote(dynamic json)
+        {    
+            Rootobject l = JsonConvert.DeserializeObject<Rootobject>(json.ToString());
 
-            return CreatedAtAction("GetReleaseNote", new { id = 11111 }, ReleaseNotes[0]);
+            for (int i = 0; i < l.releaseNoteArray.Length; i++)
+            {
+                ReleaseNote r = l.releaseNoteArray[i];
+                _context.ReleaseNote.Add(r);
+                await _context.SaveChangesAsync();
+
+            }
+            return Ok();         
         }
+
+        //// POST: api/ReleaseNote
+        //[HttpPost]
+        //public async Task<IActionResult> PostReleaseNotes([FromBody] List<ReleaseNote> ReleaseNotes)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+        //    foreach (ReleaseNote rn in ReleaseNotes)
+        //    {
+        //        _context.ReleaseNote.Add(rn);
+        //        await _context.SaveChangesAsync();
+        //    }
+
+        //    return CreatedAtAction("GetReleaseNote", new { id = 11111 }, ReleaseNotes[0]);
+        //}
 
 
         // DELETE: api/ReleaseNote/5
--- Models/ReleaseNote.cs
+++ Models/ReleaseNote.cs
@@ -1,22 +1,42 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace WebApi.Models
 {
-    public partial class ReleaseNote
+
+    public class Rootobject
     {
-        public int Id { get; set; }
+        public ReleaseNote[] releaseNoteArray { get; set; }
+    }
+
+    public class ReleaseNote 
+    {
+        public int ReleaseId { get; set; }
+        public int CleTypeId { get; set; }
         public int CountryCodeId { get; set; }
         public int EnvironmentId { get; set; }
-        public int CleTypeId { get; set; }
-        public int ReleaseId { get; set; }
         public int CommentId { get; set; }
         public string Value { get; set; }
-
+        public int Id { get; set; }
         public CleType CleType { get; set; }
         public Comment Comment { get; set; }
         public CountryCode CountryCode { get; set; }
         public Environment Environment { get; set; }
         public Release Release { get; set; }
     }
+
+
+   
+
+
+  //      public partial class ReleaseNote:ReleaseNote2
+  //  {
+  //public int Id { get; set; }
+  //      public CleType CleType { get; set; }
+  //      public Comment Comment { get; set; }
+  //      public CountryCode CountryCode { get; set; }
+  //      public Environment Environment { get; set; }
+  //      public Release Release { get; set; }
+  //  }
 }
